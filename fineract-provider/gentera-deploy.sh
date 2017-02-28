47702ff95ea55dfcb8502384c3f6be1466c70498
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2017-02-28 17:15:09 +0000

    Add deployment convenience script

---
+++ fineract-provider/gentera-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CURRENT_DIR=$(pwd)
+TARGET="root@138.197.34.190"
+
+scp $CURRENT_DIR/build/libs/fineract-provider.war $TARGET:/tmp
+ssh $TARGET chown tomcat.tomcat /tmp/fineract-provider.war -Rv
+ssh $TARGET mv /tmp/fineract-provider.war /opt/tomcat/webapps
