1fb037ca905a180c8c51e76dfa4daac20d12c517
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2017-03-01 22:11:52 +0000

    Improve deploy script

--- fineract-provider/gentera-deploy.sh
+++ fineract-provider/gentera-deploy.sh
@@ -5,4 +5,8 @@ TARGET="root@138.197.34.190"
 
 scp $CURRENT_DIR/build/libs/fineract-provider.war $TARGET:/tmp
 ssh $TARGET chown tomcat.tomcat /tmp/fineract-provider.war -Rv
+ssh $TARGET service tomcat stop
+ssh $TARGET sleep 2
+ssh $TARGET rm -rf /opt/tomcat/webapps/fineract-provider*
 ssh $TARGET mv /tmp/fineract-provider.war /opt/tomcat/webapps
+ssh $TARGET service tomcat start
