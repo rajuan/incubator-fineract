dfe7a0fcf18453193597109794d8faa0516f17a4
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2017-03-13 19:35:56 +0000

    Add convenience script to run Gentera distribution

---
+++ fineract-provider/gentera-run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CURRENT_DIR=$(pwd)
+
+cd build
+rm -rf gentera
+tar xzvf gentera.tar.gz
+cd gentera/apache-tomcat-7.0.75/bin
+./startup.sh
+tail -f ../logs/catalina.out
+
+cd $CURRENT_DIR
